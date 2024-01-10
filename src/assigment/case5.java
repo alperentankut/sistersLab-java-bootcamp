1d5655ac463d6a3d9e2f04d489f20d358cafc2b2
alperentankut <tankut.alperen@gmail.com>
2024-01-10 21:21:10 +0300

    case updated

--- src/assigment/case5.java
+++ src/assigment/case5.java
@@ -1,4 +1,27 @@
 package assigment;
 
+import java.util.Scanner;
+import java.util.Arrays;
+
 public class case5 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Birinci sayıyı girin: ");
+        int sayi1 = scanner.nextInt();
+
+        System.out.print("İkinci sayıyı girin: ");
+        int sayi2 = scanner.nextInt();
+
+        System.out.print("Üçüncü sayıyı girin: ");
+        int sayi3 = scanner.nextInt();
+
+        int[] sayilar = {sayi1, sayi2, sayi3};
+
+        Arrays.sort(sayilar);
+
+        System.out.println("Sıralanmış sayılar: " + sayilar[0] + ", " + sayilar[1] + ", " + sayilar[2]);
+
+        scanner.close();
+    }
 }
